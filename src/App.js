edde0e37b2ccb7db25578f6597346b451791363a
Cernan Bernardo <cernanb@gmail.com>
2021-04-22 21:46:39 -0700

    commit tree 6dc625256923733432529e248fea7cf495fd175b

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
-// create your App component here
+import React, { Component } from "react";
+
+class App extends Component {
+  componentDidMount() {
+    fetch("http://api.open-notify.org/astros.json")
+      .then(function (res) {
+        return res.json();
+      })
+      .then((data) => console.log(data));
+  }
+  render() {
+    return <h1>People</h1>;
+  }
+}
+
+export default App;
